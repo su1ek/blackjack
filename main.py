850c73c487e05380cb5ff2ff8b54efbd0eec67ce
Bartek <barteksulkowski7@gmail.com>
2026-06-26 23:10:35 +0200

    Initial project setup

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+suits = ['♠', '♣', '♥', '♦']
+ranks = [
+    {'rank': 'A', 'value': 11},
+    {'rank': '2', 'value': 2},
+    {'rank': '3', 'value': 3},
+    {'rank': '4', 'value': 4},
+    {'rank': '5', 'value': 5},
+    {'rank': '6', 'value': 6},
+    {'rank': '7', 'value': 7},
+    {'rank': '8', 'value': 8},
+    {'rank': '9', 'value': 9},
+    {'rank': '10', 'value': 10},
+    {'rank': 'J', 'value': 10},
+    {'rank': 'Q', 'value': 10},
+    {'rank': 'K', 'value': 10}
+]
+cards = []
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+def shuffle():
+    random.shuffle(cards)
+def deal(number):
+    cards_dealt = []
+    for a in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+shuffle()
+card = deal(1)[0]
+print(card[0], card[1]['value'])
