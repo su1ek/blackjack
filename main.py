6b6838c927e7df29451d57d69ddf6694ac43caf0
Bartek <barteksulkowski7@gmail.com>
2026-06-27 00:19:38 +0200

    Add hand class

--- main.py
+++ main.py
@@ -41,3 +41,43 @@ class Deck:
                 card = self.cards.pop()
                 cards_dealt.append(card)
         return cards_dealt
+
+class Hand:
+    def __init__(self, dealer = False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            card_value = int(card.rank['value'])
+            self.value += card_value
+            if card.rank['rank'] == 'A':
+                has_ace = True
+        if has_ace and self.value == 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def is_blackjack(self):
+        return self.get_value() == 21
+
+    def display(self, show_all_dealer_cards = False):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand''')
+        for index, card in enumerate(self.cards):
+            if index == 0 and self.dealer and not show_all_dealer_cards and not self.is_blackjack():
+                print('Hidden')
+            else:
+                print(card)
+
+        if not self.dealer:
+            print("Value:", self.get_value())
+        print()
+
