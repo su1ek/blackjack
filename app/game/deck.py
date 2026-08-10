ae7f235a174eec35baca4e264b75881544a385e3
su1ek <barteksulkowski7@gmail.com>
2026-08-10 19:06:13 +0200

    refractor: reorganize project structure

---
+++ app/game/deck.py
@@ -0,0 +1,34 @@
+class Deck:
+    def __init__(self):
+        suits = ['♠', '♣', '♥', '♦']
+        ranks = [
+            {'rank': 'A', 'value': 11},
+            {'rank': '2', 'value': 2},
+            {'rank': '3', 'value': 3},
+            {'rank': '4', 'value': 4},
+            {'rank': '5', 'value': 5},
+            {'rank': '6', 'value': 6},
+            {'rank': '7', 'value': 7},
+            {'rank': '8', 'value': 8},
+            {'rank': '9', 'value': 9},
+            {'rank': '10', 'value': 10},
+            {'rank': 'J', 'value': 10},
+            {'rank': 'Q', 'value': 10},
+            {'rank': 'K', 'value': 10}
+        ]
+        self.cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+    def deal(self, number):
+        if number > len(self.cards):
+            raise ValueError("There aren't that many cards in a deck")
+        cards_dealt = []
+        for a in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
---
+++ app/game/hand.py
@@ -0,0 +1,39 @@
+class Hand:
+    def __init__(self, dealer = False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        aces = 0
+        for card in self.cards:
+            card_value = int(card.rank['value'])
+            self.value += card_value
+            if card.rank['rank'] == 'A':
+                aces += 1
+        if aces and self.value > 21:
+            self.value -= 10
+            aces -= 1
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def is_blackjack(self):
+        return self.get_value() == 21
+
+    def display(self, show_all_dealer_cards = False):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand''')
+        for index, card in enumerate(self.cards):
+            if index == 0 and self.dealer and not show_all_dealer_cards and not self.is_blackjack():
+                print('Hidden')
+            else:
+                print(card)
+
+        if not self.dealer:
+            print("Value:", self.get_value())
+        print()
